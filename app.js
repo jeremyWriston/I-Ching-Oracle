57ec2b72bc9cbf9341560ec667fdfd4edb04109b
iktoplasm <jeremy.wriston@gmail.com>
2022-01-23 13:02:07 -0500

    Changing some unique classes to ids
    I had read that it was better to only use classes, but after some consideration and research I've decided to revert back to using id for unique elements.

---
+++ app.js
@@ -0,0 +1,13 @@
+//global variables
+
+//toss coins and create hex1 array
+
+//determine changing line and create hex2
+
+//determine binaries for hex1 and hex2
+
+//pull data from JSON based on binaries
+
+//push relevant data to DOM
+
+
